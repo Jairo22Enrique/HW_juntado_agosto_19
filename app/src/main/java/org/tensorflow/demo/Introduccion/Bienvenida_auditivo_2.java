d468068630d991cb77b54e1c2a859a3bd7e0665c
Jairo22Enrique <azulmarino-jairo22@outlook.com>
2019-08-16 22:19:39 -0500

    vas

---
+++ app/src/main/java/org/tensorflow/demo/Introduccion/Bienvenida_auditivo_2.java
@@ -0,0 +1,15 @@
+package org.tensorflow.demo.Introduccion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.tensorflow.demo.R;
+
+public class Bienvenida_auditivo_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida_auditivo_2);
+    }
+}
---
+++ app/src/main/java/org/tensorflow/demo/Introduccion/bienvenida_auditivo_1.java
@@ -0,0 +1,15 @@
+package org.tensorflow.demo.Introduccion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.tensorflow.demo.R;
+
+public class bienvenida_auditivo_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida_auditivo_1);
+    }
+}
